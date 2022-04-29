7661e40e3222856496cc1d05d873a6f7feb77262
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-30 03:30:07 +0600

    Footer component added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import { Link2 } from '../design/Link/Link';
 const Footer = () => {
   return (
     <footer>
-      <div className='container mx-auto mb-20'>
+      <div className='container mx-auto mb-20 pt-12 min-h-[160px] border-t border-solid border-gray'>
         <div className='flex justify-between items-center'>
           <div className='footer-links'>
             <Link2 className='mr-10' to='/blog' text='Contact' />
@@ -15,8 +15,19 @@ const Footer = () => {
               text='Shipping and returns'
             />
           </div>
+          <div className='news-later'>
+            <input
+              type='text'
+              name='newsletter-email'
+              id='newsletter-email'
+              placeholder='Give an email, get the newsletter'
+            />
+          </div>
         </div>
-        <div className='flex justify-between items-end'></div>
+        <div className='flex justify-between items-end'>
+          <p>&copy; {'2022'} Inventoryis. All right reserved.</p>
+          <div className='social-icons'>fb</div>
+        </div>
       </div>
     </footer>
   );
