968f136dcfdb592780a1f5d11ddf1391fa3597cb
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-05-06 21:47:54 +0600

    Added firebase.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import { Link2, Link3 } from '../design/Link/Link';
+import React from "react";
+import { Link2, Link3 } from "../design/Link/Link";
 
 const Footer = () => {
   return (
     <footer>
-      <div className='container mx-auto mb-20 min-h-[160px] border-t border-solid border-gray'>
-        <div className='min-h-[80px] flex justify-between items-end'>
-          <div className='footer-links'>
-            <Link2 className='mr-10' to='/blog' text='Contact' />
-            <Link2 className='mr-10' to='/terms' text='Terms of services' />
+      <div className="container mx-auto mb-20 min-h-[160px] border-t border-solid border-gray">
+        <div className="min-h-[80px] flex justify-between items-end">
+          <div className="footer-links">
+            <Link2 className="mr-10" to="/blog" text="Contact" />
+            <Link2 className="mr-10" to="/terms" text="Terms of services" />
             <Link2
-              className='mr-10'
-              to='/shipping'
-              text='Shipping and returns'
+              className="mr-10"
+              to="/shipping"
+              text="Shipping and returns"
             />
           </div>
-          <div className='news-later'>
+          <div className="news-later">
             <input
-              type='text'
-              name='newsletter-email'
-              id='newsletter-email'
-              placeholder='Give an email, get the newsletter'
+              type="text"
+              name="newsletter-email"
+              id="newsletter-email"
+              placeholder="Give an email, get the newsletter"
             />
           </div>
         </div>
-        <div className='min-h-[80px] flex justify-between items-end'>
+        <div className="min-h-[80px] flex justify-between items-end">
           <p>
-            &copy; {'2022'} Inventoryis.{' '}
-            <Link3 to='/privacy' text='Terms of use and privacy policy.' />
+            &copy; {"2022"} Inventoryis.{" "}
+            <Link3 to="/privacy" text="Terms of use and privacy policy." />
           </p>
-          <div className='social-icons justify-between'>fb</div>
+          <div className="social-icons justify-between">fb</div>
         </div>
       </div>
     </footer>
