42e32349108b97ba3de9172475e559b2e4c00296
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-05-16 23:06:59 +0600

    Updating latest code.

--- src/components/Inventories/Inventories.js
+++ src/components/Inventories/Inventories.js
@@ -1,36 +1,21 @@
-import React, { useEffect, useState } from "react";
-import Card from "../design/Card/Card";
-import { Link4 } from "../design/Link/Link";
-import { H1 } from "../design/Title/Title";
-
-//   {
-//     _id: 1,
-//     name: "Bread",
-//     img: "https://i.ibb.co/RvhVcKV/image14.jpg",
-//     description: "",
-//     price: "300",
-//     quantity: "",
-//     supplier: "Amir",
-//   }
+import React, { useEffect, useState } from 'react';
+import useInventory from '../../hooks/useInventory';
+import Card from '../design/Card/Card';
+import { Link4 } from '../design/Link/Link';
+import { H1 } from '../design/Title/Title';
 
 const Inventories = () => {
-  const [inventories, setInventories] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:4000/inventory")
-      .then((res) => res.json())
-      .then((data) => setInventories(data));
-  }, []);
+  const [inventory, setInventory] = useInventory();
 
   return (
-    <section className="container mx-auto">
-      <div className="section-header flex justify-between items-center">
-        <H1 className="mt-16" text="Inventory" />
-        <Link4 className="mt-16" to="/inventory" text="View all" />
+    <section className='container mx-auto'>
+      <div className='section-header flex justify-between items-center'>
+        <H1 className='mt-16' text='Inventory' />
+        <Link4 className='mt-16' to='/inventory' text='View all' />
       </div>
 
-      <div className="w-full my-4 grid grid-cols-3 gap-20">
-        {inventories.map((el) => (
+      <div className='w-full my-4 grid grid-cols-3 gap-20'>
+        {inventory.map((el) => (
           <Card key={el._id} item={el} />
         ))}
       </div>
